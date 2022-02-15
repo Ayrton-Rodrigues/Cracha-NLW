e9fc02a2c40c08d9e2556ce14f064b0e092ef5b5
Ayrton Rodrigues <ayrton.hce@gmail.com>
2022-02-15 20:53:58 -0300

    Correcao de bug

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-       // function showmesomething() {
-         //   alert(links.instagram)
-       // }
-// objeto \/
 const linksSocialMedia = {
-    github: "Ayrtonrodri",
+    github: "Ayrton-Rodrigues",
     youtube: "cursoemvideo",
     facebook: "jessica amaral",
     instagram: "Ayrtonrodri",
@@ -12,34 +8,27 @@ const linksSocialMedia = {
     
     function changeSocialMediaLinks(){
         for(let li of socialLinks.children)
-    {const social = li.getAttribute('class')
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    {
+        const social = li.getAttribute('class')
     
-    }
-    
-    
-    //ser humano conta a partir do 1 e o pc a partir de 0.
-    
-    //for(let i = 0; i <= 10; i++)
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
        
+
     } 
-    
+}
     changeSocialMediaLinks()
-     //const github = "Ayrtonrodri"
-    //const youtube = "cursoemvideo"
-    //const facebook = "Jessica amaral"
-    //const instagram = "Ayrtonrodri"
-    //const twitter = "Ayrtonnot"
-    
-    //showmesomething()
+
+
+
     
     function getGitHubProfileInfos() {
         const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
         fetch(url)
         .then(Response => Response.json())
-        .then(data => { userBio.textContent = data.bio 
-            userName.textContent = data.name
+        .then(data => 
+            {                       
+            userName.textContent = data.name           
             userLink.href = data.html_url
             userImage.src = data.avatar_url
             userLogin.textContent = data.login
